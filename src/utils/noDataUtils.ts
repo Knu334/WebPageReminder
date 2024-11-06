04ffd4e5561dc225d2d7023af0f9c7e4d9f922c1
Knu334 <who.is.fuu@gmail.com>
2024-11-06 20:41:47 +0900

    テストコミット2

--- src/utils/noDataUtils.ts
+++ src/utils/noDataUtils.ts
@@ -10,6 +10,7 @@ export const NO_DATA = {
   autoOpen: false,
   webPush: false,
   createdAt: new Date().toISOString(),
+  aaa: ""
 } as const satisfies Reminder;
 
 export const delNodata = (reminders: Reminder[]): Reminder[] => {
